d3beb1c614455dcd12b5403e0fdf7502c6bfcd68
SeungWoo Yoo <dreamy1534@gmail.com>
2021-01-15 18:33:35 +0900

    feat(main): Add 'julia_set.cpp'

--- src/julia_set.cpp
+++ src/julia_set.cpp
@@ -3,9 +3,15 @@
 
 void Julia(Complex p, Complex c, int& i, float& r) {
 	int maxIterations = 256;
+	float rSqr;
 
 	for (i = 0; i < maxIterations; i++) {
 		Complex p = p * p + c;
-		float rSqr = p.re * p.re + p.im * p.im;
+		rSqr = p.re * p.re + p.im * p.im;
+
+		if (rSqr > 4)
+			break;
 	}
+	 
+	r = sqrt(rSqr);
 }
--- src/opengl-crash-course.cpp
+++ src/opengl-crash-course.cpp
@@ -35,6 +35,7 @@ int main(int argc, char** argv) {
 
 	glutCreateWindow("First Screen");
 	CheckError("Failed to create window");
+
 	glutDisplayFunc(DrawCircle);
 	glutMainLoop();
 }
